36e237d56067d9e324cc30fef217453efd712e10
Luke Aiken <lukeaiken@gmail.com>
2014-09-16 14:18:49 -0600

    postgres setup

--- config.ru
+++ config.ru
@@ -5,4 +5,10 @@ Bundler.require
 
 require 'app'
 
+SliceWorksApp.db = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/slice_works')
+
+database = Sequel.postgres('slice_works')
+database.run "CREATE TABLE IF NOT EXISTS people (id serial primary key, name varchar(255))"
+database.run "INSERT INTO people(name) VALUES('George')"
+
 run SliceWorksApp
--- lib/app.rb
+++ lib/app.rb
@@ -1,16 +1,31 @@
 require 'sequel'
 require_relative './slice_works'
 
+#Sequel.postgres('slice_works')
+
 class SliceWorksApp < Sinatra::Base
+  attr_reader :db
+
   set :method_override, true
   set :root, 'lib/app'
   set :views, settings.root + '/views'
   set :public_folder, File.dirname(__FILE__) + '/public'
 
-
   Tilt.register Tilt::ERBTemplate, 'html.erb'
 
+  enable :sessions
+
+  class << self
+    attr_accessor :db
+  end
+
+  before do
+    @db = self.class.db
+  end
+
   get '/' do
+    people = db.fetch "select * from people"
+    @person = people.to_a.first
     erb :home
   end
 
