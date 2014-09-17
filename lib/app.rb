576e1e88d866fb77f58eb27780752b69a22e233b
Jessica Goulding <jgoulding2@gmail.com>
2014-09-17 14:17:54 -0600

    added routes for cms integration
    added home_layout, moved nav bar partial
    
    added views for goodbye, login, etc

--- lib/app.rb
+++ lib/app.rb
@@ -21,7 +21,7 @@ class SliceWorksApp < Sinatra::Base
   get '/' do
     people = db[:people].select
     @person = people.to_a.first
-    erb :home
+    erb :home, layout: :home_layout
   end
 
   not_found do
@@ -29,9 +29,10 @@ class SliceWorksApp < Sinatra::Base
   end
 
   get '/:location/menu/:menu_type' do
-    erb params[:menu_type].to_sym 
+    erb params[:menu_type].to_sym
   end
 
+  ###Editing Menu DB###
 
   get '/admin' do
     @menu_items = db[:menu_items]
@@ -46,21 +47,114 @@ class SliceWorksApp < Sinatra::Base
     db.run("UPDATE menu items SET name = #{params[:name]}, description = #{params[:description]}, price = #{params[:price]} WHERE id = #{id}")
   end
 
-  get '/admin/locations' do
-    @locations = Sequel.get('locations')
-    @locations = [{id: 1, name: 'Denver Location', address: '123'},
-                  {id: 2, name: 'Littleton Location', address: '456'}]
-    erb 'admin/locations'
+  ###Content Pages###
+
+  get '/admin/pages/new' do
+    @page = Page.new
+    slim :new
   end
 
-  get '/admin/locations/:id/edit' do |id|
-    @location = Sequel.get('location', id)
-    erb 'admin/locations/edit'
+  post 'admin/pages' do
+    page = Page.create(params[:page])
+    redirect to("/pages/#{page.id}")
+  end
+
+  get '/pages/:id/edit' do
+    @page = Page.find(params[:id])
+    slim :edit
+  end
+
+  put '/pages/:id' do
+    page = Page.find(params[:id])
+    page.update_attributes(params[:page])
+    redirect to("/pages/#{page.id}")
+  end
+
+  get '/pages/delete/:id' do
+    @page = Page.find(params[:id])
+    slim :delete
+  end
+
+  delete '/pages/:id' do
+    Page.find(params[:id]).destroy
+    redirect to('/pages')
+  end
+
+  get '/login' do
+    haml :login
+  end
+
+  post '/login' do
+    authenticate!
+    if session[:user] == "admin"
+      redirect '/admin_dashboard'
+    else
+      redirect '/login'
+    end
+  end
+
+  ###LogIn and CMS Functionality###
+
+  get '/admin_dashboard' do
+    login_helper(:admin_dashboard)
+  end
+
+  get '/edit_menu' do
+    login_helper(:admin_menu)
+  end
+
+  get '/edit/:item_id' do |item_id|
+    login_helper(:form, {item_id: item_id})
+  end
+
+  post '/edit/:item_id' do |item_id|
+    Menu.new.edit(item_id, params) if authenticated?
+    redirect '/admin_dashboard'
+  end
+
+  get '/delete/:item_id' do |item_id|
+    Meunu.new.delete(item_id) if authenticated?
+    redirect '/admin_dashboard'
+  end
+
+  get '/add_menu_item' do
+    haml :add_form
+  end
+
+  post '/add' do
+    Menu.new.add(params) if authenticated?
+    redirect '/admin_dashboard'
+  end
+
+  get '/logout' do
+    session[:user] = nil
+    redirect '/goodbye'
+  end
+
+  ###Authentication Helpers###
+  helpers do
+    def authenticate!
+      if params[:user] == "admin" && params[:password] == "password"
+        session[:user] = "admin"
+      end
+    end
+
+    def authenticated?
+      session[:user] == "admin" ? true : false
+    end
+
+    def login_helper(view, locals = {})
+      if authenticated?
+        haml view, locals: locals
+      else
+        redirect '/login'
+      end
+    end
   end
 
   get '/:slug' do |slug|
     if slug == "lodo"
-      number = '(303 297-3464)'
+      number = '(303) 297-3464'
       erb :capitol_hill, :locals => {:number => number}
     elsif slug == 'capitol_hill'
       number = '(303) 993-8127'
@@ -69,6 +163,18 @@ class SliceWorksApp < Sinatra::Base
     erb slug.to_sym
     end
   end
+
+  # get '/admin/locations' do
+  #   @locations = Sequel.get('locations')
+  #   @locations = [{id: 1, name: 'Denver Location', address: '123'},
+  #                 {id: 2, name: 'Littleton Location', address: '456'}]
+  #   erb 'admin/locations'
+  # end
+  #
+  # get '/admin/locations/:id/edit' do |id|
+  #   @location = Sequel.get('location', id)
+  #   erb 'admin/locations/edit'
+  # end
 
   # post '/admin/locations/:id' do |id|
   #   @location = Sequal.get('location', id)
